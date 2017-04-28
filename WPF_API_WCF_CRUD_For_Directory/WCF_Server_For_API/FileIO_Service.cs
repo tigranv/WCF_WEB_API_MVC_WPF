e9108bb7ea3f981b18d2de0bd1885c4e8f33e808
Tigran Vardanyan <vardanyant.89@gmail.com>
2017-04-29 00:02:40 +0400

    Finished

---
+++ WPF_API_WCF_CRUD_For_Directory/WCF_Server_For_API/FileIO_Service.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCF_Server_For_API
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class FileIO_Service : IServiceIO
+    {
+        public void Delete(string dirName, string fileName)
+        {
+            string p = Path.Combine("D:\\", dirName);
+
+            string path = Path.Combine(p, fileName);
+
+            string[] Files = Directory.GetFiles(@p);
+
+            foreach (string file in Files)
+            {
+                if (file.ToUpper().Contains(fileName.ToUpper()))
+                {
+                    File.Delete(file);
+                }
+            }
+        }
+
+        public string Get(string dirName, string fileName)
+        {
+            string p = Path.Combine("D:\\", dirName);
+
+            string path = Path.Combine(p, fileName);
+            StreamReader sr = File.OpenText(path);
+            string textline = sr.ReadLine();
+            sr.Close();
+            return textline;
+        }
+
+        public IEnumerable<string> GetAllFiles(string dirName)
+        {
+            string p = Path.Combine("D:\\", dirName);
+            DirectoryInfo directory = new DirectoryInfo(p);
+            var files = directory.GetFiles().Where(f => !f.Attributes.HasFlag(FileAttributes.Hidden)).Select(f => f.Name).ToArray();
+            return files;
+        }
+
+        public void Post(string dirName, string fileName, string value)
+        {
+            string p = Path.Combine("D:\\", dirName);
+            string path = Path.Combine(p, fileName);
+            StreamWriter sw = new StreamWriter(path);
+            sw.Write(value);
+            sw.Close();
+        }
+
+        public void Put(string dirName, string fileName, string value)
+        {
+            string p = Path.Combine("D:\\", dirName);
+            string path = Path.Combine(p, fileName);
+            StreamWriter sw = new StreamWriter(path);
+            sw.Write(value);
+            sw.Close();
+        }
+    }
+}
--- WPF_API_WCF_CRUD_For_Directory/WebApi_Client_For_WCF/Controllers/MainController.cs
+++ WPF_API_WCF_CRUD_For_Directory/WebApi_Client_For_WCF/Controllers/MainController.cs
@@ -6,69 +6,37 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using System.Web.Script.Serialization;
-
+using WebApi_Client_For_WCF.ServiceReference1;
 
 namespace WebApi_Client_For_WCF.Controllers
 {
     public class MainController : ApiController
     {
+        ServiceIOClient proxy = new ServiceIOClient();
         // GET: api/Main
         public IEnumerable<string> GetAllFiles(string dirName)
         {
-            string p = Path.Combine("D:\\", dirName);
-            DirectoryInfo directory = new DirectoryInfo(p);
-            var files = directory.GetFiles().Where(f => !f.Attributes.HasFlag(FileAttributes.Hidden)).Select(f => f.Name).ToArray();
-            return files;
+            return proxy.GetAllFilesAsync(dirName).Result;
         }
 
-        // "D:\TestDirectory" 
-
-        // GET: api/Main/5
         public string Get(string dirName, string fileName)
         {
-            string p = Path.Combine("D:\\", dirName);
-
-            string path = Path.Combine(p, fileName);
-            StreamReader sr = File.OpenText(path);
-            string textline = sr.ReadLine();
-            sr.Close();
-            return textline;
+            return proxy.GetAsync(dirName, fileName).Result;
         }
 
         public void Put([FromUri]string dirName, [FromUri]string fileName, [FromBody]string value)
         {
-            string p = Path.Combine("D:\\", dirName);
-            string path = Path.Combine(p, fileName);
-            StreamWriter sw = new StreamWriter(path);
-            sw.Write(value);
-            sw.Close();
+            proxy.PutAsync(dirName, fileName, value);
         }
 
         public void Post([FromUri]string dirName, [FromUri]string fileName, [FromBody]string value)
         {
-            string p = Path.Combine("D:\\", dirName);
-            string path = Path.Combine(p, fileName);
-            StreamWriter sw = new StreamWriter(path);
-            sw.Write(value);
-            sw.Close();
+            proxy.PostAsync(dirName, fileName, value);
         }
 
-        // DELETE: api/Main/5
         public void Delete(string dirName, string fileName)
         {
-            string p = Path.Combine("D:\\", dirName);
-
-            string path = Path.Combine(p, fileName);
-
-            string[] Files = Directory.GetFiles(@p);
-
-            foreach (string file in Files)
-            {
-                if (file.ToUpper().Contains(fileName.ToUpper()))
-                {
-                    File.Delete(file);
-                }
-            }
+            proxy.DeleteAsync(dirName, fileName);
         }
     }
 }
