e7f71f9e831bb685787117f8929b502929a99bfd
Tigran Vardanyan <vardanyant.89@gmail.com>
2017-05-05 01:29:05 +0400

    Some starting

--- BetRayerChatHttp.v.2.0/WcfServiceLibraryForChat/IChatContract.cs
+++ BetRayerChatHttp.v.2.0/WcfServiceLibraryForChat/IChatContract.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace WcfServiceLibraryForChat
 {
@@ -12,9 +13,9 @@ namespace WcfServiceLibraryForChat
     public interface IChatContract
     {
         [OperationContract]
-        void Send(Message message);
+        void AddSend(Message message);
         [OperationContract]
-        Task<List<Message>> GetMessages();
+        Task<List<Message>> GetAllMessages();
         [OperationContract]
         Task<bool> LoginAsync(User user);
         [OperationContract]
@@ -24,8 +25,7 @@ namespace WcfServiceLibraryForChat
         [OperationContract]
         List<User> GetAllUsersAsync();
         [OperationContract]
-        Task<List<User>> GetAllUsersAsyncFromDb();
-        [OperationContract]
+        
         void RemoveFromList(User users);
 
         // TODO: Add your service operations here
@@ -34,23 +34,20 @@ namespace WcfServiceLibraryForChat
     // Use a data contract as illustrated in the sample below to add composite types to service operations.
     // You can add XSD files into the project. After building the project, you can directly use the data types defined there, with the namespace "WcfServiceLibraryForChat.ContractType".
     [DataContract]
-    public class CompositeType
+    public class Message
     {
         bool boolValue = true;
         string stringValue = "Hello ";
 
-        [DataMember]
-        public bool BoolValue
-        {
-            get { return boolValue; }
-            set { boolValue = value; }
-        }
+        
+    }
 
-        [DataMember]
-        public string StringValue
-        {
-            get { return stringValue; }
-            set { stringValue = value; }
-        }
+    [DataContract]
+    public class User
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+       
     }
 }
