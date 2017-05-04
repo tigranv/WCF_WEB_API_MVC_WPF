dcb5b5dae4d01cf8eb2acb3d81c98c30b2e20fe3
Tigran Vardanyan <vardanyant.89@gmail.com>
2017-05-05 01:29:38 +0400

    some corrections

--- BetRayerChatHttp.v.2.0/WcfServiceLibraryForChat/IChatService.cs
+++ BetRayerChatHttp.v.2.0/WcfServiceLibraryForChat/IChatService.cs
@@ -8,24 +8,8 @@ using System.Text;
 namespace WcfServiceLibraryForChat
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
-    public class Service1 : IService1
+    public class IChatService : IChatContract
     {
-        public string GetData(int value)
-        {
-            return string.Format("You entered: {0}", value);
-        }
-
-        public CompositeType GetDataUsingDataContract(CompositeType composite)
-        {
-            if (composite == null)
-            {
-                throw new ArgumentNullException("composite");
-            }
-            if (composite.BoolValue)
-            {
-                composite.StringValue += "Suffix";
-            }
-            return composite;
-        }
+      
     }
 }
