61a9599bfd9bafd0647164c694ab61e8d63ef895
Tigran Vardanyan <vardanyant.89@gmail.com>
2017-05-08 09:27:54 +0400

    crud system cleanded

--- CRUD For File System(WPF to WEB.API)/File Manipulator WPF/MainWindow.xaml.cs
+++ CRUD For File System(WPF to WEB.API)/File Manipulator WPF/MainWindow.xaml.cs
@@ -1,23 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.IO;
-using System.Linq;
-using System.Net;
 using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
-using System.Web;
 using System.Web.Script.Serialization;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Windows.Threading;
 
 namespace File_Manipulator_WPF
--- CRUD For File System(WPF to WEB.API)/REST For FileSystem API/Controllers/DirectoryController.cs
+++ CRUD For File System(WPF to WEB.API)/REST For FileSystem API/Controllers/DirectoryController.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
 
 namespace REST_For_FileSystem_API.Controllers
